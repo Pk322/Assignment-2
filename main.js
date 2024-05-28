0009313959ec8765fb71d98db24b060352f261f8
Pk322 <160255216+Pk322@users.noreply.github.com>
2024-05-28 21:33:39 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// Assignment No:01
+/*.Personal Message: Store a person’s name
+in a variable, and print a message to that person.
+ Your message should be simple, such as, “Hello Eric,
+ would you like to learn some Python today?”*/
+//Store a person’s name in a variable
+let personName = "Eric";
+//and print a message to that person.
+console.log(`“Hello ${personName} would you like to learn some Python today?” `);
